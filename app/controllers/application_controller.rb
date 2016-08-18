803e464cac0811d9cdc6316e04809b1d9765d1a0
ozPop <ozziepopovas@gmail.com>
2016-08-18 15:30:09 -0400

    add layout files & custom.css, enable sessions & views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ require './config/environments'
 
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-
+  configure do
+    set :session_secret, "secret"
+    enable :sessions
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+  
   get '/' do
-    "Hello World"
+    erb :'/application/index'
   end
 
 end
