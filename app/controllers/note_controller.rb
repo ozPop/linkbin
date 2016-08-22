96a76e0bee9c2abc31a0ad35a7633b253834aff7
ozPop <ozziepopovas@gmail.com>
2016-08-22 17:23:41 -0400

    add patch action to note_controller

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -53,8 +53,8 @@ class NoteController < ApplicationController
   end
 
   get '/notes/:id/edit' do
-    if logged_in? && current_user(session).id == Note.find(params[:id]).user_id
-      @note = Note.find(params[:id])
+    @note = Note.find(params[:id])
+    if logged_in? && current_user(session).id == @note.user_id
       erb :'/notes/edit'
     else
       redirect '/'
@@ -62,7 +62,52 @@ class NoteController < ApplicationController
   end
 
   patch '/notes/:id/edit' do
+    # binding.pry
+    if params[:topic_ids].nil? && params[:topic][:name].empty?
+      flash[:message] = "You have to select or create a topic!"
+      redirect "/notes/#{params[:id]}/edit"
+    else
+      note = Note.find(params[:id])
+      note.description = params[:description]
+      note.links = params[:links]
+      note.content = params[:content]
+      note.public_access = params.has_key?("public")
+      note.user_id = session[:user_id]
+      # add updated time field to db and display that
+      note.date = Time.now
+      note.save
+      # The logic below is extremely bad !!!!
+      # currently removing all associations and repopulating the DB
+      # should update when possible delete where needed or add new ones if none exist
+      unless params[:topic_ids].nil?
+        note.topics.clear
+        topics = params[:topic_ids].map do |id|
+          topic = Topic.find(id)
+          NoteTopic.create(note_id: note.id, topic_id: topic.id)
+        end
+      end
+      unless params[:topic][:name].empty?
+        if Topic.find_by(name: params[:topic][:name])
+          flash[:message] = "The topic already exists, please select it"
+          reditect '/notes/new'
+        else
+          note.topics.clear
+          topic = Topic.create(params[:topic])
+          NoteTopic.create(note_id: note.id, topic_id: topic.id)
+        end
+      end
+    end
+    flash[:message] = "Success!"
+    redirect "/users/#{current_user(session).slug}"
+  end
 
+  get '/notes/:id/delete' do
+    note = Note.find(params[:id])
+    if logged_in? && current_user(session).id == note.user_id
+      note.destroy
+      flash[:message] = "Successfully Deleted Item"
+    end
+    redirect '/'
   end
 
 end
