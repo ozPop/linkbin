3607d23b40b404088b2b146fe4de5f0cb40ea69d
ozPop <ozziepopovas@gmail.com>
2016-08-24 15:58:40 -0400

    refactor note_controller, date & time displaying, edit view, close #2
    refactor notes patch in note_controller
    fix date & time displaying to local time
    update /notes/edit view params

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -52,40 +52,25 @@ class NoteController < ApplicationController
   end
 
   patch '/notes/:id/edit' do
-    # binding.pry
-    if params[:topic_ids].nil? && params[:topic][:name].empty?
+    if params[:topic][:names].nil?
       flash[:message] = "You have to select or create a topic!"
       redirect "/notes/#{params[:id]}/edit"
     else
+      params[:note]["date_updated"] = Time.now
+      params[:note]["user_id"] = current_user(session).id
       note = Note.find(params[:id])
-      note.description = params[:description]
-      note.links = params[:links]
-      note.content = params[:content]
-      note.public_access = params.has_key?("public")
-      note.user_id = session[:user_id]
-      # add updated time field to db and display that
-      note.date = Time.now
-      note.save
-      # The logic below is extremely bad !!!!
+      note.update(params[:note])
+      note.topics.clear
       # currently removing all associations and repopulating the DB
-      # should update when possible delete where needed or add new ones if none exist
-      unless params[:note][:topic_ids].nil?
-        note.topics.clear
-        topics = params[:note][:topic_ids].map do |id|
-          topic = Topic.find(id)
-          NoteTopic.create(note_id: note.id, topic_id: topic.id)
-        end
-      end
-      unless params[:topic][:name].empty?
-        if Topic.find_by(name: params[:topic][:name])
-          flash[:message] = "The topic already exists, please select it"
-          reditect '/notes/new'
+      # should use update
+      params[:topic][:names].each do |n|
+        if Topic.find_by(name: n.downcase)
+          note.topics << Topic.find_by(name: n.downcase)
         else
-          note.topics.clear
-          topic = Topic.create(params[:topic])
-          NoteTopic.create(note_id: note.id, topic_id: topic.id)
+          note.topics.build(name: n.downcase) if n != ""
         end
       end
+      note.save
     end
     flash[:message] = "Success!"
     redirect "/users/#{current_user(session).slug}"
